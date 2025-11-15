6787862c04a4bb2082849ba88f7aa5dfd865c5b5
360Parminder <yesask8@gmail.com>
2025-11-16 00:53:21 +0530

    Enable optional authentication in submission server

--- src/submission.js
+++ src/submission.js
@@ -33,7 +33,7 @@ function buildServer() {
     size: Number(MAX_SIZE),
     secure: false,
     disabledCommands: ['STARTTLS'], // allow STARTTLS if you terminate TLS at proxy set to ["STARTTLS"]
-    authOptional: false,
+    authOptional: true,
  socketTimeout: 2 * 60 * 1000,
     // Only AUTH PLAIN/LOGIN allowed by default; restrict mechanisms if needed
    onAuth: async (auth, session, callback) => {
